0acffa1b07789937a97b40b0ecae640b1049957a
Iqzah <iqzahfarhah@gmail.com>
2026-08-07 03:57:57 +0800

    Update dashboard.php

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -2,7 +2,7 @@
 
 session_start();
 
-require_once "../database/db.php";
+require_once "database/db.php";
 
 
 if(!isset($_SESSION['user_id']) || $_SESSION['role']!="admin"){
@@ -111,7 +111,7 @@ Admin Dashboard
 </title>
 
 
-<link rel="stylesheet" href="../assets/css/admin.css">
+<link rel="stylesheet" href="assets/css/admin.css">
 
 
 </head>
