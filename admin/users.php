a5323345c720d21da09132b82728a04f7258b16a
Iqzah <iqzahfarhah@gmail.com>
2026-08-07 01:07:54 +0800

    Update users.php

--- admin/users.php
+++ admin/users.php
@@ -5,68 +5,13 @@ session_start();
 require_once "../config/db.php";
 
 
+if($_SESSION['role']!="admin"){
 
-if(!isset($_SESSION['user_id'])){
-
-    header("Location: ../login.php");
-    exit();
-
-}
-
-
-
-$user_id=$_SESSION['user_id'];
-
-
-
-$admin=$conn->query("
-
-SELECT role
-
-FROM users
-
-WHERE user_id='$user_id'
-
-")->fetch_assoc();
-
-
-
-if($admin['role']!="admin"){
-
-    exit("Access denied");
-
-}
-
-
-
-
-
-
-if(isset($_GET['delete'])){
-
-
-$id=$_GET['delete'];
-
-
-
-$conn->query("
-
-DELETE FROM users
-
-WHERE user_id='$id'
-
-");
-
-
-header("Location: users.php");
+header("Location: ../index.php");
 
 exit();
 
-
 }
-
-
-
 
 
 
@@ -78,9 +23,7 @@ FROM users
 
 ORDER BY user_id DESC
 
-
 ");
-
 
 
 ?>
@@ -97,27 +40,12 @@ Manage Users
 </title>
 
 
-<link rel="stylesheet" href="../assets/css/style.css">
-
 <link rel="stylesheet" href="../assets/css/admin.css">
-
 
 </head>
 
 
 <body>
-
-
-
-<?php include "../includes/navbar.php"; ?>
-
-
-
-<section class="dashboard-page">
-
-
-<div class="dashboard-container">
-
 
 
 <h1>
@@ -126,34 +54,22 @@ Users Management
 
 
 
-
-<table class="admin-table">
+<table border="1">
 
 
 <tr>
 
-<th>
-ID
-</th>
+<th>ID</th>
 
-<th>
-Name
-</th>
+<th>Name</th>
 
-<th>
-Email
-</th>
+<th>Email</th>
 
-<th>
-Role
-</th>
+<th>Role</th>
 
-<th>
-Action
-</th>
+<th>Status</th>
 
 </tr>
-
 
 
 
@@ -165,76 +81,47 @@ Action
 
 <td>
 
-<?php echo $row['user_id']; ?>
+<?= $row['user_id']; ?>
 
 </td>
-
 
 
 <td>
 
-<?php echo $row['name']; ?>
+<?= htmlspecialchars($row['name']); ?>
 
 </td>
-
-
 
 
 <td>
 
-<?php echo $row['email']; ?>
+<?= htmlspecialchars($row['email']); ?>
 
 </td>
-
-
 
 
 <td>
 
-<?php echo $row['role']; ?>
+<?= $row['role']; ?>
 
 </td>
-
-
 
 
 <td>
 
-
-<a href="users.php?delete=<?php echo $row['user_id']; ?>"
-onclick="return confirm('Delete user?')">
-
-Delete
-
-</a>
-
-
+<?= $row['status']; ?>
 
 </td>
-
 
 
 </tr>
 
 
-
 <?php } ?>
-
 
 
 </table>
 
-
-
-</div>
-
-
-</section>
-
-
-
-
-<?php include "../includes/footer.php"; ?>
 
 
 </body>
