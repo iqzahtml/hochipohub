3f443c282a863ed206b5405ba60ce5efc738eb1d
Iqzah <iqzahfarhah@gmail.com>
2026-08-07 01:08:13 +0800

    Update remove_wishlist.php

--- ajax/remove_wishlist.php
+++ ajax/remove_wishlist.php
@@ -8,77 +8,21 @@ require_once "../config/db.php";
 header("Content-Type: application/json");
 
 
-
-
-
-if(!isset($_SESSION['user_id'])){
-
-
-echo json_encode([
-
-"status"=>"error",
-
-"message"=>"Login required"
-
-]);
-
-
-exit();
-
-
-}
-
-
-
-
-
 $user_id=$_SESSION['user_id'];
 
-
-
-$product_id=$_POST['product_id'] ?? '';
-
-
-
-
-
-if(empty($product_id)){
-
-
-
-echo json_encode([
-
-"status"=>"error",
-
-"message"=>"Product ID missing"
-
-]);
-
-
-exit();
-
-
-}
-
-
-
-
+$product_id=$_POST['product_id'];
 
 
 
 $stmt=$conn->prepare("
 
-
 DELETE FROM wishlist
 
-
-WHERE user_id=? 
+WHERE user_id=?
 
 AND product_id=?
 
-
 ");
-
 
 
 $stmt->bind_param(
@@ -93,44 +37,14 @@ $product_id
 
 
 
-if($stmt->execute()){
+$stmt->execute();
 
 
 
 echo json_encode([
 
-
-"status"=>"success",
-
-
-"message"=>"Removed from wishlist"
-
-
+"status"=>"success"
 
 ]);
-
-
-
-}
-
-else{
-
-
-echo json_encode([
-
-
-"status"=>"error",
-
-
-"message"=>"Failed to remove"
-
-
-
-]);
-
-
-}
-
-
 
 ?>
