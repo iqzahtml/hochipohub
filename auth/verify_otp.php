1d245d81ad2f7d9a0736faef0828ffe2b8f27a8b
Iqzah <iqzahfarhah@gmail.com>
2026-08-07 00:15:31 +0800

    Update verify_otp.php

---
+++ auth/verify_otp.php
@@ -0,0 +1,88 @@
+<?php
+
+session_start();
+
+require_once "../config/db.php";
+
+
+
+$email=$_SESSION['otp_email'] ?? '';
+
+$otp=$_POST['otp'];
+
+
+
+$stmt=$conn->prepare("
+
+
+SELECT *
+
+FROM users
+
+WHERE email=?
+
+AND otp=?
+
+
+");
+
+
+$stmt->bind_param(
+
+"ss",
+
+$email,
+
+$otp
+
+);
+
+
+
+$stmt->execute();
+
+
+
+$result=$stmt->get_result();
+
+
+
+if($result->num_rows==1){
+
+
+$user=$result->fetch_assoc();
+
+
+
+if(strtotime($user['otp_expiry']) >= time()){
+
+
+$_SESSION['reset_email']=$email;
+
+
+header("Location: ../reset_password.php");
+
+
+exit();
+
+
+
+}
+
+
+
+}
+
+
+
+$_SESSION['error']="Invalid OTP";
+
+
+header("Location: ../verify_otp.php");
+
+
+exit();
+
+
+
+?>
