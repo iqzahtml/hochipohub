0e8859de96b216511d4c5da01f91ea3241dfe99a
Iqzah <iqzahfarhah@gmail.com>
2026-08-07 03:45:45 +0800

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -2,7 +2,7 @@
 
 session_start();
 
-require_once "../database/db.php";
+require_once "database/db.php";
 
 
 if(!isset($_SESSION['user_id'])){
@@ -106,7 +106,7 @@ Dashboard
 </title>
 
 
-<link rel="stylesheet" href="../assets/css/style.css">
+<link rel="stylesheet" href="css/style.css">
 
 
 </head>
