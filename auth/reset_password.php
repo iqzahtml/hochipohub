a27fdc4cf3180840d6d1f0abe3b80d3a048c93ea
Iqzah <iqzahfarhah@gmail.com>
2026-08-08 03:26:58 +0800

    Update reset_password.php

--- auth/reset_password.php
+++ auth/reset_password.php
@@ -1,77 +1,171 @@
 <?php
 
-session_start();
+require_once "../config.php";
 
-require_once "database/db.php";
+require_once "../database/db.php";
 
+require_once "../includes/functions.php";
 
-
-if(isset($_POST['reset'])){
-
-
-$email=$_SESSION['reset_email'];
-
-
-$password=$_POST['password'];
+require_once "../includes/session.php";
 
 
 
-$new=password_hash(
-
-$password,
-
-PASSWORD_DEFAULT
-
-);
 
 
-
-$stmt=$conn->prepare("
-
-
-UPDATE users
-
-SET password=?,
-
-otp=NULL,
-
-otp_expiry=NULL
+/*
+|--------------------------------------------------------------------------
+| Check OTP Verification
+|--------------------------------------------------------------------------
+*/
 
 
-WHERE email=?
+if(
+    !isset($_SESSION['otp_verified'])
+    ||
+    !isset($_SESSION['reset_email'])
+){
 
 
-");
+    header(
+
+        "Location: forgot_password.php"
+
+    );
 
 
+    exit();
 
-$stmt->bind_param(
 
-"ss",
-
-$new,
-
-$email
-
-);
+}
 
 
 
-$stmt->execute();
+
+
+$email = $_SESSION['reset_email'];
 
 
 
-unset($_SESSION['reset_email']);
 
 
 
-$_SESSION['success']="Password changed";
+/*
+|--------------------------------------------------------------------------
+| Reset Password
+|--------------------------------------------------------------------------
+*/
 
 
-header("Location: login.php");
+if($_SERVER['REQUEST_METHOD']=="POST"){
 
 
-exit();
+
+    $password = $_POST['password'];
+
+    $confirmPassword = $_POST['confirm_password'];
+
+
+
+
+
+    if($password !== $confirmPassword){
+
+
+
+        setFlashMessage(
+
+            "error",
+
+            "Password does not match."
+
+        );
+
+
+
+    }else{
+
+
+
+        $newPassword = password_hash(
+
+            $password,
+
+            PASSWORD_DEFAULT
+
+        );
+
+
+
+
+
+
+        $update = $conn->query("
+
+
+        UPDATE users
+
+
+        SET password='$newPassword'
+
+
+        WHERE email='$email'
+
+
+        ");
+
+
+
+
+
+
+
+        if($update){
+
+
+
+            /*
+            |--------------------------------------------------------------------------
+            | Clear Reset Session
+            |--------------------------------------------------------------------------
+            */
+
+
+            unset($_SESSION['reset_email']);
+
+            unset($_SESSION['otp_verified']);
+
+
+
+
+
+            setFlashMessage(
+
+                "success",
+
+                "Password successfully changed. Please login."
+
+            );
+
+
+
+
+            header(
+
+                "Location: ".BASE_URL."index.php"
+
+            );
+
+
+            exit();
+
+
+
+
+        }
+
+
+
+    }
 
 
 }
@@ -79,3 +173,124 @@ exit();
 
 
 ?>
+
+
+
+<?php include "../includes/header.php"; ?>
+
+
+
+<section class="auth-page">
+
+
+
+<div class="auth-box">
+
+
+
+<h1>
+
+Create New Password
+
+</h1>
+
+
+
+
+<form method="POST">
+
+
+
+
+
+<div class="form-group">
+
+
+<label>
+
+New Password
+
+</label>
+
+
+
+<input
+
+type="password"
+
+name="password"
+
+required
+
+>
+
+
+
+</div>
+
+
+
+
+
+
+
+<div class="form-group">
+
+
+<label>
+
+Confirm Password
+
+</label>
+
+
+
+<input
+
+type="password"
+
+name="confirm_password"
+
+required
+
+>
+
+
+
+</div>
+
+
+
+
+
+
+
+<button
+
+type="submit"
+
+class="btn-primary"
+
+>
+
+Reset Password
+
+</button>
+
+
+
+
+</form>
+
+
+
+
+</div>
+
+
+
+</section>
+
+
+
+<?php include "../includes/footer.php"; ?>
