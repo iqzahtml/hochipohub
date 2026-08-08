0eed00739a4c0f0e05f470582becc435a32a26f0
Iqzah <iqzahfarhah@gmail.com>
2026-08-09 00:35:23 +0800

    Update forgot_password.php

--- auth/forgot_password.php
+++ auth/forgot_password.php
@@ -1,99 +1,54 @@
 <?php
 
 require_once "../config.php";
-
 require_once "../database/db.php";
-
 require_once "../includes/functions.php";
 
-
-$pageTitle="Forgot Password";
-
+$pageTitle = "Forgot Password";
 
 ?>
 
-
 <?php include "../includes/header.php"; ?>
-
-
 
 <section class="auth-page">
 
+    <div class="auth-box">
 
-<div class="auth-box">
+        <h1>Forgot Password</h1>
 
+        <p>
+            Enter your registered email address to receive a reset code.
+        </p>
 
-<h1>
+        <form action="send_otp.php" method="POST">
 
-Forgot Password
+            <div class="form-group">
 
-</h1>
+                <label for="email">
+                    Email
+                </label>
 
+                <input
+                    type="email"
+                    id="email"
+                    name="email"
+                    required
+                    autocomplete="email"
+                >
 
-<p>
+            </div>
 
-Enter your email to receive OTP.
+            <button
+                type="submit"
+                class="btn-primary"
+            >
+                Send OTP
+            </button>
 
-</p>
+        </form>
 
-
-
-
-
-<form action="send_otp.php" method="POST">
-
-
-
-<div class="form-group">
-
-
-<label>
-
-Email
-
-</label>
-
-
-<input
-
-type="email"
-
-name="email"
-
-required
-
->
-
-
-</div>
-
-
-
-
-
-<button
-
-class="btn-primary"
-
-type="submit"
-
->
-
-Send OTP
-
-</button>
-
-
-
-</form>
-
-
-
-</div>
-
+    </div>
 
 </section>
-
-
 
 <?php include "../includes/footer.php"; ?>
