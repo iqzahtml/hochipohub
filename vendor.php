362aefbf43f07205bb2f18639840039b8b3a9880
Afiq <afiqaizad188996@gmail.com>
2026-08-18 09:06:58 +0800

    Update vendor.php

--- vendor.php
+++ vendor.php
@@ -187,6 +187,7 @@ if ($vendorId > 0) {
 |--------------------------------------------------------------------------
 */
 
+$extraCSS = ['vendor.css'];
 require_once __DIR__ . '/includes/header.php';
 require_once __DIR__ . '/includes/navbar.php';
 
