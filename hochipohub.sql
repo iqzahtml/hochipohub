96a2592c1f59631528e3b0afeade3814b733ea2a
Iqzah <iqzahfarhah@gmail.com>
2026-08-23 21:48:37 +0800

    Update hochipohub.sql

--- hochipohub.sql
+++ hochipohub.sql
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 -- =========================================================
 -- HOCHIPOHUB DATABASE
 -- =========================================================
@@ -715,6 +716,8 @@ SELECT
 FROM users
 WHERE email = 'admin@hochipohub.com';hochipohubhochipohubusers
 =======
+=======
+>>>>>>> 6695cf635ac6f08caf77cc07108163b42ea8b8d8
 -- --------------------------------------------------------
 -- Host:                         127.0.0.1
 -- Server version:               8.0.30 - MySQL Community Server - GPL
@@ -1036,4 +1039,7 @@ CREATE TABLE IF NOT EXISTS `wishlist` (
 /*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
+<<<<<<< HEAD
+>>>>>>> 6695cf635ac6f08caf77cc07108163b42ea8b8d8
+=======
 >>>>>>> 6695cf635ac6f08caf77cc07108163b42ea8b8d8
