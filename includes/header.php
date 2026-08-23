cb31ac15f6c7fcd12df11f98bf846744fdbd3ccf
Afiq <afiqaizad188996@gmail.com>
2026-08-23 21:35:43 +0800

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -402,6 +402,10 @@ $allJS = array_unique($allJS);
             'UTF-8'
         ) ?>"
     >
+    <link
+    rel="stylesheet"
+    href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css"
+>
 
 
     <!-- =====================================================
